166a8d51cd5ba4fd1ad5cd0b168b5a5954e6fd42
senanayake <janith@edhirya.com>
2023-12-08 14:53:24 +0530

    initital commit

---
+++ src/test/java/com/developeriqtracker/commitmanagementservice/CommitManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developeriqtracker.commitmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
